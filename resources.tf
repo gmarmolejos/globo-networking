b037a53ae5a1c361d3d13dae728228464e6acb6b
Gabriel Marmolejos <gmarmolejos@unipago.com.do>
2024-04-19 19:48:30 -0400

    Remove Security Group

--- resources.tf
+++ resources.tf
@@ -42,25 +42,3 @@ module "main" {
 
   tags = local.common_tags
 }
-
-resource "aws_security_group" "ingress" {
-  description = "Security group with no ingress rule"
-  egress = [{
-    cidr_blocks      = ["0.0.0.0/0"]
-    description      = ""
-    from_port        = 0
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    protocol         = "-1"
-    security_groups  = []
-    self             = false
-    to_port          = 0
-  }]
-  ingress                = []
-  name                   = "no-ingress-sg"
-  name_prefix            = null
-  revoke_rules_on_delete = null
-  tags                   = local.common_tags
-  tags_all               = {}
-  vpc_id                 = module.main.default_vpc_id
-}
